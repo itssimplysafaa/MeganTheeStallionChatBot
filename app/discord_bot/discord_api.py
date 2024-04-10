722aec9e2a25b4ad98cb84f31fc87b77e66f201e
Safa Rasheed <safarasheed02@gmail.com>
2024-04-09 22:27:43 -0400

    started implementation of discord bot

---
+++ app/discord_bot/discord_api.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import discord
+import os
+
+load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print("Successfully logged in as:", self.user)
+    
+    async def on_message(self, message):
+        print(message.content)
+        if message.author == self.user:
+            return
+        
+        command, user_message = None, None
+        for text in ['/ai', '/bot', '/chatgpt']:
+            if message.content.startswith(text):
+                command = message.content.split(' ')[0]
+                user_message = message.content.replace(text, '')
+                print(command, user_message)
+
+        if command in ['/ai', '/bot', '/chatgpt']:
+            bot_response = chatgpt_response(prompt=user_message)
+            await message.channel.send("Answer: {bot_response}" )
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
